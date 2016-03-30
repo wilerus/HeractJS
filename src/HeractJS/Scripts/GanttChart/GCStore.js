96c4c70d1c10ed5a6620762a632b3d19e03d5d35
Vladislav Smirnov <vdmsmirnov@gmail.com>
2016-03-30 13:18:17 +0300

    Gantt chart: update git ignore

--- src/HeractJS/Scripts/GanttChart/GCStore.js
+++
@@ -1,9 +0,0 @@
-var globalStore = (function () {
-    function globalStore() {
-    }
-    globalStore.svgGridWidth = 50;
-    globalStore.cellCapacity = 24;
-    globalStore.cellSize = 50 / 24;
-    return globalStore;
-})();
-exports.globalStore = globalStore;
