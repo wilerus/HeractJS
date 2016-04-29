6dcafee5421ebd21a05eb2d5a931da96b35b80ca
Vladislav Smirnov <vdmsmirnov@gmail.com>
2016-04-29 18:13:28 +0300

    Gantt view: clean-up

--- src/HeractJS/Scripts/GanttChart/Timeline.ts
+++ src/HeractJS/Scripts/GanttChart/Timeline.ts
@@ -1,4 +1,4 @@
-﻿import React = require('react')
+﻿import * as React from 'react';
 
 export class Timeline extends React.Component<any, any> {
     constructor() {
--- src/HeractJS/typings/main.d.ts
+++ src/HeractJS/typings/main.d.ts
@@ -2,5 +2,4 @@
 /// <reference path="browser/ambient/react-redux/index.d.ts" />
 /// <reference path="browser/ambient/react/index.d.ts" />
 /// <reference path="browser/ambient/redux/index.d.ts" />
-/// <reference path="browser/ambient/require/index.d.ts" />
 /// <reference path="browser/definitions/debug/index.d.ts" />
