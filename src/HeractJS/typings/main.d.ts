60d9b5d1ce9b79444853a1bd80021fc7d941d1e9
Vladislav Smirnov <vdmsmirnov@gmail.com>
2016-05-17 10:52:43 +0300

    Gantt view:libs update

--- src/HeractJS/typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/react-dom/index.d.ts" />
-/// <reference path="browser/ambient/react-redux/index.d.ts" />
-/// <reference path="browser/ambient/react/index.d.ts" />
-/// <reference path="browser/ambient/redux/index.d.ts" />
-/// <reference path="browser/definitions/debug/index.d.ts" />
