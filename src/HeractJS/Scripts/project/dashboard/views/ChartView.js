3dfd9198a476902e754d7afa530b00730ee613ea
Ilya Kulikov <Ilya.Kulikov@comindware.com>
2016-10-03 19:18:21 +0300

    Half-working example of filling dashboard with simple text.

---
+++ src/HeractJS/Scripts/project/dashboard/views/ChartView.js
@@ -0,0 +1,10 @@
+﻿define([
+    'comindware/core'
+], function () {
+    return Marionette.ItemView.extend({
+        initialize: function () {
+        },
+        template: Handlebars.compile("<div class = chart-test-region>some text</div>")
+    })
+})
+            
