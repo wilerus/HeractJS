2ac1ea7bbc69ef2848a7cf059997ec25bace8e98
Stanislav Guryev <Stanislav.Guryev@comindware.com>
2016-10-06 14:15:50 +0300

    Dashboard: selection navigation items added

--- src/HeractJS/Scripts/project/dashboard/views/DashboardNavigationView.js
+++ src/HeractJS/Scripts/project/dashboard/views/DashboardNavigationView.js
@@ -6,26 +6,46 @@
     'use strict';
 
     return function () {
-        var collection = new Backbone.Collection([{ id: 'chart1', title: 'Chart1' }, { id: 'chart2', title: 'Chart2' }]);
+        var collection = new Backbone.Collection([{ id: 'chart1', title: 'Chart1', selected: true }, { id: 'chart2', title: 'Chart2', selected: false }]);
         
         var ListItemView = Marionette.ItemView.extend({
+            initialize: function () {
+                if (this.model.attributes.selected) {
+                    (this.$el.addClass('dasboard-navigation-item-selected'));
+                }  
+            },    
             template: Handlebars.compile(template),
-            className: 'navigationButton',
+            className: 'dashboard-navigation-button',
             events: {
                 click: function (e) {
                     this.trigger('chartChanged', e)
                 }
-            }
+            },
+
         });
 
         var ListView = Marionette.CollectionView.extend({
             childView: ListItemView,
             collection: collection,
-            className: 'navigationContainer',
+            className: 'dashboard-navigation-container',
             childEvents: {
                 'chartChanged': function (e) {
+                    this.changeSelection(e);
                     this.triggerMethod('chartChanged', e);
                 }
+            },
+            changeSelection: function (e) {
+                var views = this.children._views;
+                for (var i in views) {
+                    var selected = views[i].model.attributes.selected;
+                    if (selected) {
+                        views[i].model.attributes.selected = false;
+                        views[i].$el.removeClass('dasboard-navigation-item-selected');
+                        break
+                    }
+                }
+                e.model.attributes.selected = true;
+                e.$el.addClass('dasboard-navigation-item-selected');
             }
         });
 
