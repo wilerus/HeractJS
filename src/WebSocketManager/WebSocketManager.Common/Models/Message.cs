4fd2aa2fdb84644b92f9bfa1c87f444234aa443e
DmitryLyange <dimliang95@gmail.com>
2017-10-13 11:12:02 +0300

    Model update

--- src/WebSocketManager/WebSocketManager.Common/Models/Message.cs
+++ src/WebSocketManager/WebSocketManager.Common/Models/Message.cs
@@ -3,13 +3,24 @@
     public enum MessageType
     {
         Text,
-        ClientMethodInvocation,
         ConnectionEvent
     }
 
-    public class Message
+    public struct Message
     {
         public MessageType MessageType { get; set; }
+
         public string Data { get; set; }
+
+        public string Id { get; set; }
+    }
+
+    public struct MessageData
+    {
+        public string Message { get; set; }
+
+        public string UserName { get; set; }
+
+        public string DateTime { get; set; }
     }
 }
